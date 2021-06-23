abc4a26bc8120c6a40ab7a79bf3fecaf9b9bda84
ricky <rra.rickyresky@gmail.com>
2021-06-23 11:50:01 +0800

    ds

--- example_file.php
+++ example_file.php
@@ -1,7 +1,9 @@
 <?php
 require 'src/mega.class.php';
 
-$megafile = new MEGA('https://mega.co.nz/#!yl5EFARS!c6T1en1P8N9GuQzsMy5iCh2U9NEmuqTvSd4KkW42UX4');
+// $megafile = new MEGA('https://mega.co.nz/#!yl5EFARS!c6T1en1P8N9GuQzsMy5iCh2U9NEmuqTvSd4KkW42UX4');
+$megafile = new MEGA('https://mega.nz/#!ef51ALII!ArlQarBqBCa_TSOFVM86Sw');
+// $megafile = new MEGA('https://mega.nz/folder/ef51ALII#ArlQarBqBCa_TSOFVM86Sw/file/qXZhXKwJ');
 
 #$megafile->download_file(); // to download as files
 $megafile->download_zip(); // to download as zip. Make sure you have enough space for file in the root path of project
--- example_folder.php
+++ example_folder.php
@@ -2,7 +2,8 @@
 //ini_set('display_errors',true);
 require_once('src/mega.class.php');
 
-$megafile = new MEGA('https://mega.nz/#F!6ghFGDyD!uVPZiTXJTuDh_c5-cAevrw');
-
+// $megafile = new MEGA('https://mega.nz/#F!6ghFGDyD!uVPZiTXJTuDh_c5-cAevrw');
+$megafile = new MEGA('https://mega.nz/folder/ef51ALII#ArlQarBqBCa_TSOFVM86Sw');
+#$megafile->download_file(); // to download as files
 $megafile->download_zip(); // to download using streams. Make sure you have enough space for folder in /tmp
 ?>
